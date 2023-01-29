9c6e43133673bc7849eb11fff19b2ac239192077
jamiromic <jamiromic@gmail.com>
2023-01-29 17:19:04 +0100

    add exercise 10

--- Exercise_4/src/it/test/Esercizio10.java
+++ Exercise_4/src/it/test/Esercizio10.java
@@ -3,13 +3,26 @@ package it.test;
 public class Esercizio10 {
 
 	public static void main(String[] args) {
+		int sommaTotale = 27;
+		int numeroMisterioso = 0;
+		int numero1 = 0;
+		int numero2 = 0;
+		int numero3 = 0;
+		int numero4 = 0;
+		
+			for(int i = 0; i < 10; i++) {
+				int numero = i;
+				if(numero % 2 != 0) {
+					numero4 = numero;
+				}
+			}
 		
 
 	}
-
+	
 }
 
-
+//Numero misterioso è 9837
 
 //Holy digits Batman! L'Enigmista sta pianificando un nuovo crimine da qualche parte sulla Pennsylvania Avenue. come al solito, ha lasciato l'indirizzo sotto forma di enigma.
 //Il numero civico della Pennsylvania Avenue è formato da 4 cifre che soddisfano le seguenti condizioni:
