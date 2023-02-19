95ca2160b2d8fd0ceb7d90083da92bbd0035267d
jamir <jamir@192.168.1.4>
2023-02-19 21:25:34 +0100

    add exercises

--- Test/src/test/it/AnagrammiTest.java
+++ Test/src/test/it/AnagrammiTest.java
@@ -1,32 +1,35 @@
 package test.it;
 
-
+import java.util.Arrays;
 
 public class AnagrammiTest {
 	
 	public static boolean anagramma(int array1[], int array2[]) {
-		boolean contains = false;
-		int contatoreUguaglianze = 0;
-		for (int i = 0; i < array1.length; i++) {
-			for (int j = 0; j < array2.length; j++) {
-				if(array2[j] == array1[i]) {
-					contatoreUguaglianze++;
-				} 
-			}
-			if(contatoreUguaglianze == array2.length) {
-				contains = true;
-			}
-		}
-   	 return contains;
-    }
-	
+		boolean sonoUguali = false;
+        //Controllo se i due array hanno la stessa lunghezza
+		if (array1.length != array2.length) {
+            sonoUguali = false;
+        }
+        else {
+        	//Ordino gli interi all'interno dei due array in ordine crescente
+            Arrays.sort(array1);
+            Arrays.sort(array2);
+            //Confronto i due array ordinati
+            if (Arrays.equals(array1, array2)) {
+                sonoUguali = true;
+            }
+            else {
+                sonoUguali = false;
+            }
+        }
+		return sonoUguali;
+	}
 
+		
 	public static void main(String[] args) {
-		int array1[] = {1,2,3,6,6,1};
-		int array2[] = {1,3,2,6,6,3};
-		System.out.println(AnagrammiTest.anagramma(array1,array2));
-	     
-	     
+		int array1[] = {3,2,1,6,6,1};
+		int array2[] = {1,3,2,6,6,1};
+		System.out.println(AnagrammiTest.anagramma(array1,array2));     
 
 	}
 
